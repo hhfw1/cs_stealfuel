295ecd29fe2b17cdcbbb0d2a2257cd05b3a8e2f7
hhfw1 <cchoudhary168@gmail.com>
2022-09-23 17:49:47 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@
 CodeStudio = {}
 
 
-CodeStudio.FuelType = 'abrp_fuel'  --Your Fuel Sysytem LegacyFuel/ps-fuel or if you have any other add here
+CodeStudio.FuelType = 'LegacyFuel'  --Your Fuel Sysytem LegacyFuel/ps-fuel or if you have any other add here
 CodeStudio.StealTime = 10    --Time to Steal in seconds
 CodeStudio.RefuellingTime = 10   --Time to Refuel Fuel in seconds
 CodeStudio.ExplodeOnFail = true  ---Explode Vehicle on Failing to do mini game
@@ -18,7 +18,7 @@ CodeStudio.Circles = math.random(1,2) --Amount of Circle for the minigame
 ----SHOPS-----
 
 CodeStudio.EnableShops = true  ---Enable Disable Shops
-CodeStudio.Target = 'abrp_target' 
+CodeStudio.Target = 'qb-target' 
 CodeStudio.ShopPed = 's_m_m_movprem_01'
 CodeStudio.Location = {    ---Shop Location add as many as you want
     vector4(299.04, -578.75, 43.26, 91),
@@ -27,7 +27,7 @@ CodeStudio.ToolItems = {  ---Shop Items add whatever you want
     label = 'Gasoline Shop',
     slots = 30,
     items = {
-        { name = "mechanic_tools", price = 15000, amount = 10, info = {}, type = "item", slot = 1,},
+        { name = "empty_can", price = 0, amount = 100, info = {}, type = "item", slot = 1,},
     },
 }
 
